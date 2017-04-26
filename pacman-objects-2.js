757b3e62de0c963ec6224f7642e043e57fae5cf6
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-04-26 07:56:41 -0400

    More practice with js objects

---
+++ pacman-objects-2.js
@@ -0,0 +1,12 @@
+var ghosts = {inky: 'Cyan', blinky: 'Red', pink: 'Pink', clyde: 'Orange'};
+
+console.log(ghosts);
+
+console.log(ghosts.inky);
+console.log(ghosts.blinky);
+
+console.log(ghosts['inky']);
+console.log(ghosts['blinky']);
+
+var ghostName = 'clyde';
+console.log(ghosts[ghostName]);
